986aeb6969b11f6d52b0db32b4bb3c570e5d1bc3
Krishna Arora <krishna.arora.02007@gmail.com>
2026-06-15 20:58:57 +0530

    style: make captions smaller, white text, solid black background, closer to bottom

--- src/football_pipeline/moviepy_edit.py
+++ src/football_pipeline/moviepy_edit.py
@@ -108,7 +108,7 @@ def build_moviepy_edit(
     in_name = temp_output.name
     out_name = output_path.name
     
-    style = "Fontsize=28,PrimaryColour=&H00FFFF&,OutlineColour=&H80000000&,BorderStyle=1,Outline=2,Alignment=2,MarginV=120"
+    style = "Fontsize=20,PrimaryColour=&H00FFFFFF&,BackColour=&H00000000&,BorderStyle=3,Outline=0,Shadow=0,Alignment=2,MarginV=80"
     command = [
         ffmpeg_exe, "-y", 
         "-i", in_name, 
